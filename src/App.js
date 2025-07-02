89aa9b705b5e9701bae3d3d850472eb930b8360c
TarotF <fcperez@student.hau.edu.ph>
2025-07-02 22:25:00 +0800

    only put dis call function in App.js

--- src/App.js
+++ src/App.js
@@ -1,80 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
+import ChickenBananaGame from './BCgame';
 import './App.css';
-import { useState } from 'react';
-import Grid from './grid';
-const imageUrls = [
-  'https://thumbs.dreamstime.com/b/bunch-bananas-6175887.jpg?w=768',
-  'https://thumbs.dreamstime.com/z/full-body-brown-chicken-hen-standing-isolated-white-backgroun-background-use-farm-animals-livestock-theme-49741285.jpg?ct=jpeg',
-  'https://thumbs.dreamstime.com/b/bunch-bananas-6175887.jpg?w=768',
-  'https://thumbs.dreamstime.com/z/full-body-brown-chicken-hen-standing-isolated-white-backgroun-background-use-farm-animals-livestock-theme-49741285.jpg?ct=jpeg',
-  'https://thumbs.dreamstime.com/b/bunch-bananas-6175887.jpg?w=768',
-  'https://thumbs.dreamstime.com/z/full-body-brown-chicken-hen-standing-isolated-white-backgroun-background-use-farm-animals-livestock-theme-49741285.jpg?ct=jpeg',
 
-];
-
-function getRandomImage() {
-  const index = Math.floor(Math.random() * imageUrls.length);
-  return imageUrls[index];
-}
-
-
-function Welcome (props){
-  return <h2>Welcome, {props.name}</h2>;
-}
-
-
-function Counter(){
-  const[count, setCount]= useState(0);
-
-  function handleClick(){
-  setCount(count +1 );
-  
-  }
-
-
-  return(
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={handleClick}>Click me</button>
-    </div>
-  )
-}
-
-
-function Tables(){
-  <div class = "container">
-  <div>1</div>
-  <div>2</div>
-  <div>3</div>  
-  <div>4</div>
-  <div>5</div>
-  <div>6</div>  
-  <div>7</div>
-  <div>8</div>
-  </div>
-
-}
-
-export default function App() {
-  const gridBase = {
-    cells: 6,
-    rows: 6
-  };
-
-  const [grid, setGrid] = useState(gridBase);
- 
-
-  
-
+function App() {
   return (
-    <div className="app">
-      <Grid
-        grid={grid}
-        
-        
-      />
+    <div className="App">
+      <ChickenBananaGame />
     </div>
   );
 }
 
-
+export default App;
